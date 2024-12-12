0f2f5a5484d217b822d99a3579a732b5f8ea9dd4
wooco <96woosj@gmail.com>
2024-12-13 01:01:17 +0900

    fix: gradle 의존성 추가, config 추가

--- src/main/java/com/bangchef/recipe_platform/security/SecurityConfig.java
+++ src/main/java/com/bangchef/recipe_platform/security/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.bangchef.recipe_platform.security;
-
 import com.bangchef.recipe_platform.security.token.repository.RefreshTokenRepository;
 import com.bangchef.recipe_platform.user.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -21,41 +19,30 @@ public class SecurityConfig {
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JWTUtil jwtUtil;
     private final RefreshTokenRepository refreshTokenRepository;
-
     public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil,
                           RefreshTokenRepository refreshTokenRepository, UserRepository userRepository) {
-
         this.authenticationConfiguration = authenticationConfiguration;
         this.jwtUtil = jwtUtil;
         this.refreshTokenRepository = refreshTokenRepository;
         this.userRepository = userRepository;
-
     }
-
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
             throws Exception {
-
         return configuration.getAuthenticationManager();
     }
-
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
                 .csrf((auth) -> auth.disable());
-
         http
                 .formLogin((auth) -> auth.disable()); //
-
         http
                 .httpBasic((auth) -> auth.disable());
-
         http
                 .authorizeHttpRequests((auth) -> auth
 
@@ -67,31 +54,30 @@ public class SecurityConfig {
                                 "/users/login",
                                 "/users/join",
                                 "users/verify/**",
+                                "/users/verify/**",
+                                "/users/reset-password",
+                                "/users/info",
+                                "/users/update",
                                 "/recipes/**",
                                 "/h2-console/**" //서브경로 포함
                         ).permitAll()
 
                         .anyRequest().authenticated());
-
         http
                 .headers(headers -> headers.frameOptions(frame -> frame.disable())); // 프레임 옵션 비활성화
-
         http
                 .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil,
                                 refreshTokenRepository, userRepository),
                         UsernamePasswordAuthenticationFilter.class);
-
         http
                 .addFilterBefore(new JWTFilter(userRepository, jwtUtil),
                         UsernamePasswordAuthenticationFilter.class);
-
         http
                 .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshTokenRepository),
                         LogoutFilter.class);
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
         return http.build();
     }
 }
